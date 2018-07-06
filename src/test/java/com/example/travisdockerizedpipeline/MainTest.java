bec01d48b750eb1ba0c98457bcb44cd974094857
woznihack <wozmind@gmail.com>
2018-07-06 13:58:32 +0200

    tests fix to make travis pass

--- src/test/java/com/example/travisdockerizedpipeline/MainTest.java
+++ src/test/java/com/example/travisdockerizedpipeline/MainTest.java
@@ -7,8 +7,8 @@ import static org.junit.Assert.assertEquals;
 public class MainTest {
     @Test
     public void testOne(){
-        System.out.println("This is a failing test");
-        assertEquals(true, false);
+        System.out.println("This is now a successful test");
+        assertEquals(true, true);
     }
 
     @Test
